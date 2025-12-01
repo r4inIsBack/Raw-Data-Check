c708f03b86e9a20ccd2561e7d7a8fc91e13c8e19
R4IN <logged2fa@gmail.com>
2025-11-30 23:52:51 -0600

    Update Package.php

--- InfoBase/Data/Package.php
+++ InfoBase/Data/Package.php
@@ -1,5 +1,3 @@
 #EXTM3U
-#EXTINF:-1 type="radio" tvg-name="KXHT" tvg-logo="logo", HOT1071 TRU HIP-HOP
-https://securenetg.com/listen/hot1071/hot.aac
-#EXTINF:-1 tvg-name="KXHT" tvg-logo="logo" radio="true", HOT1071 TRU HIP-HOP
+#EXTINF:-1 tvg-name="KXHT" radio="true", HOT1071 TRU HIP-HOP
 https://securenetg.com/listen/hot1071/hot.aac
