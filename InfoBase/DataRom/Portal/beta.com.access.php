0902c9fd9a2c1bd6641c933a64155241ee526507
R4IN <logged2fa@gmail.com>
2025-12-13 10:37:34 -0600

    Update beta.com.access.php

--- InfoBase/DataRom/Portal/beta.com.access.php
+++ InfoBase/DataRom/Portal/beta.com.access.php
@@ -1,14 +1,11 @@
 #EXTM3U
-#EXTINF:-1 group-title="GigaRed" tvg-logo="logo", Fox Sports
-#KODIPROP:inputstream.adaptive.manifest_type=mpd
-#KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key={"6c0016fff15fcc3cc5d103f821189100":"94d789ac388ccbbfbd2cf91d5b7db028"}
+
+#EXTINF:-1 tvg-logo="logo" group-title="GigaRed", Fox Sports
+#DRM:cbc
+#KEY:6c0016fff15fcc3cc5d103f821189100:94d789ac388ccbbfbd2cf91d5b7db028
 https://cdnlb.gigared.com.ar/live/eds/Fox_Sports/sa_live_dash/Fox_Sports.mpd?0GKUSH_R4IN&DONTSTEAL
-#EXTINF:-1 group-title="GigaRed" tvg-logo="logo", Telefe
-#EXTVLCOPT:http-user-agent=Mozilla/5.0
-#EXTVLCOPT:http-referrer=https://www.gigared.com.ar/
-#EXTVLCOPT:http-origin=https://www.gigared.com.ar
-#EXTVLCOPT:inputstream-adaptive-manifest-type=mpd
-#EXTVLCOPT:inputstream-adaptive-license-type=clearkey
-#EXTVLCOPT:inputstream-adaptive-license-key={"865b35fdb4c119f3ccfd4ee392928aef":"b43adf6d88b3befbf7f69a71dc273e46"}
+
+#EXTINF:-1 tvg-logo="logo" group-title="GigaRed", Telefe
+#DRM:cbc
+#KEY:865b35fdb4c119f3ccfd4ee392928aef:b43adf6d88b3befbf7f69a71dc273e46
 https://cdnlb.gigared.com.ar/live/eds/Telefe/sa_live_dash/Telefe.mpd?0GKUSH_R4IN&DONTSTEAL
