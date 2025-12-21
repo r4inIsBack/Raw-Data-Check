987ad82a24d45e0e54e8449f2e3ce8d87db519d3
R4IN <logged2fa@gmail.com>
2025-12-20 19:32:07 -0600

    Create Test OTT.php

---
+++ InfoBase/DataRom/Portal/Test OTT.php
@@ -0,0 +1,6 @@
+#EXTM3U
+#EXTINF:-1 group-title="FLOW ARGENTINA" tvg-logo="logo", Telefe
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"3cec1b1ea9799dda5596e64f37e5ed20":"c69f3afde2085dcaaaddbf55246a0323"}
+https://chromecast.cvattv.com.ar/live/c6eds/TelefeHD/SA_Live_dash_enc_C/TelefeHD.mpd
