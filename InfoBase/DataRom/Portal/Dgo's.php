f847e265fbe7d45db3f48457fe608ad4ebaf2f09
R4IN <logged2fa@gmail.com>
2025-12-23 16:32:39 -0600

    Create Dgo's.php

---
+++ InfoBase/DataRom/Portal/Dgo's.php
@@ -0,0 +1,6 @@
+#EXTM3U
+#EXTINF:-1 group-title="DGO" tvg-logo="logo", Discovery Turbo
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"914a9207cb025a548daff3b69a58bbaa":"07924a9102cb545a8daff3b69a58bbaa"}
+http://atlas.cdmx.gob.mx:8089/https://1081-tbx.vrioott.com/discoveryturbo/manifest.mpd?0GKUSH_R4IN&DONTSTEAL
