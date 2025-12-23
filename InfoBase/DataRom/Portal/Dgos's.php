5e21aa9e5fa7a504161953f158b30d586002569b
R4IN <logged2fa@gmail.com>
2025-12-23 16:34:08 -0600

    Update and rename Dgo's.php to Dgos's.php

--- InfoBase/DataRom/Portal/Dgos's.php
+++ InfoBase/DataRom/Portal/Dgos's.php
@@ -2,5 +2,5 @@
 #EXTINF:-1 group-title="DGO" tvg-logo="logo", Discovery Turbo
 #KODIPROP:inputstream.adaptive.manifest_type=mpd
 #KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key={"914a9207cb025a548daff3b69a58bbaa":"07924a9102cb545a8daff3b69a58bbaa"}
+#KODIPROP:inputstream.adaptive.license_key={"914a9207cb025a548daff3b69a58bbaa":"8045ad134fa4f627e25e65363a224853"}
 http://atlas.cdmx.gob.mx:8089/https://1081-tbx.vrioott.com/discoveryturbo/manifest.mpd?0GKUSH_R4IN&DONTSTEAL
