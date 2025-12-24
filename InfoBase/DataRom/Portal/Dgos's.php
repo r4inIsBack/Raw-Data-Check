16671e4d45af1bce74d08293db72f119592d6226
R4IN <logged2fa@gmail.com>
2025-12-24 14:24:25 -0600

    Delete InfoBase/DataRom/Portal/Dgos's.php

--- InfoBase/DataRom/Portal/Dgos's.php
+++
@@ -1,6 +0,0 @@
-#EXTM3U
-#EXTINF:-1 group-title="DGO" tvg-logo="logo", Discovery Turbo
-#KODIPROP:inputstream.adaptive.manifest_type=mpd
-#KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key={"914a9207cb025a548daff3b69a58bbaa":"8045ad134fa4f627e25e65363a224853"}
-http://atlas.cdmx.gob.mx:8089/https://1081-tbx.vrioott.com/discoveryturbo/manifest.mpd?0GKUSH_R4IN&DONTSTEAL
