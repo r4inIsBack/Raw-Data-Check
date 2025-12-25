67b1503471c22e1b091926a21da0155fe9e45b89
R4IN <logged2fa@gmail.com>
2025-12-25 16:11:43 -0600

    Create prueba.php

---
+++ InfoBase/DataRom/Portal/prueba.php
@@ -0,0 +1,61 @@
+#EXTM3U
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Acocina
+https://cdn.mic.net.pe/ACOCINA/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Comercio TV
+https://cdn.mic.net.pe/COMERCIO_TV/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", DmKidsTV
+https://cdn.mic.net.pe/DM_KIDS/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Eco TV
+https://cdn.mic.net.pe/ECO_TV/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", EnerGeek Fan
+https://cdn.mic.net.pe/ENERGEEK_FAN/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo",  Esports Max
+https://cdn.mic.net.pe/ESPORT_MAX/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo",  Esports Max 2
+https://cdn.mic.net.pe/ESPORT_MAX_2/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", FHM Movies
+https://cdn.mic.net.pe/FMH_MOVIES/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", FMH Family
+https://cdn.mic.net.pe/FMH_FAMILY/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", FMH Kids
+https://cdn.mic.net.pe/FMH_KIDS/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Gracia TV
+https://cdn.mic.net.pe/GRACIA/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Inka Vision
+https://cdn.mic.net.pe/INKA_VISION/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Kanade
+https://cdn.mic.net.pe/KANADE/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Max Anime
+https://cdn.mic.net.pe/MAX_ANIME/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Memorias Classic
+https://cdn.mic.net.pe/MEMORIAS_CLASSIC_DIRECTO/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Memorias Film
+https://cdn.mic.net.pe/MEMORIAS_FILM_DIRECTO/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Memorias Series
+https://cdn.mic.net.pe/MEMORIAS_SERIES_DIRECTO/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Memorias Toons
+https://cdn.mic.net.pe/MEMORIAS_TOONS_DIRECTO/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Memorias Tropical
+https://cdn.mic.net.pe/MEMORIAS_TROPICAL_DIRECTO/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Memorias TV
+https://cdn.mic.net.pe/MEMORIAS_TV_DIRECTO/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Nativa
+https://cdn.mic.net.pe/NATIVA/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", NeoTV
+https://cdn.mic.net.pe/Neotv/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Ovacion
+https://cdn.mic.net.pe/OVACION/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Pasiones
+https://cdn.mic.net.pe/PASIONES/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Piura TV
+https://cdn.mic.net.pe/PIURA_TV/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Ruidos TV
+https://cdn.mic.net.pe/RUIDOS_TV/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Suyai TV
+https://cdn.mic.net.pe/SUYAI_TV/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", TVM Chile
+https://cdn.mic.net.pe/TVM/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", Zkids
+https://cdn.mic.net.pe/ZKIDS/index.m3u8?0GKUSH_R4IN&DONTSTEAL
+#EXTINF:-1 group-title="MIC PE" group-logo="https://r4inisback.github.io/tweb/logos/mic-pe.png" tvg-logo="logo", ZTV
+https://cdn.mic.net.pe/ZTV/index.m3u8?0GKUSH_R4IN&DONTSTEAL 
