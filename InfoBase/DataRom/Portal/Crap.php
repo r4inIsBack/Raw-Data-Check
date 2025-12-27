a1244aac9d81def0ac80a8d9c891ca5cc45439dd
R4IN <logged2fa@gmail.com>
2025-12-27 04:27:25 -0600

    Update and rename Crap.php to Craps.php

--- InfoBase/DataRom/Portal/Crap.php
+++
@@ -1,5 +0,0 @@
-#EXTM3U
-#EXTINF:-1 group-title="KKK NIGGERS" group-logo="logo" tvg-logo="logo", TSN1
-#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
-#KODIPROP:inputstream.adaptive.license_key=https://license.9c9media.ca/widevine
-http://138.197.129.179:7000/https://live-de.video.9c9media.com/f/ctv/usanetwork/manifest.mpd
---
+++ InfoBase/DataRom/Portal/Craps.php
@@ -0,0 +1,5 @@
+#EXTM3U
+#EXTINF:-1 group-title="KKK NIGGERS" group-logo="logo" tvg-logo="logo", USA
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=https://license.9c9media.ca/widevine
+https://cors-proxy.cooks.fyi/https://live-de.video.9c9media.com/f/ctv/usanetwork/manifest.mpd
