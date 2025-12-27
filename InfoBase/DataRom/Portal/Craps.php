ee54bf84c87c3ffa5ed0f76173269b02d8d56266
R4IN <logged2fa@gmail.com>
2025-12-27 04:40:51 -0600

    Update Craps.php

--- InfoBase/DataRom/Portal/Craps.php
+++ InfoBase/DataRom/Portal/Craps.php
@@ -1,5 +1,5 @@
 #EXTM3U
-#EXTINF:-1 group-title="KKK NIGGERS" group-logo="logo" tvg-logo="logo", USA
+#EXTINF:-1 tvg-name="FOX-QRO"
 #KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
-#KODIPROP:inputstream.adaptive.license_key=https://license.9c9media.ca/widevine
-https://cors-proxy.cooks.fyi/https://live-de.video.9c9media.com/f/ctv/usanetwork/manifest.mpd
+#KODIPROP:inputstream.adaptive.license_key=0c39778fe8fb4f9f8574016b3d4ae2ed:232b1f450f2e37f47fdd5b678a8a546d
+http://147.78.1.28/xview/fox_tvpay_qro_hd.mpd
