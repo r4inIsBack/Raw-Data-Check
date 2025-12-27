ebbc025e4ac38447ce596daf81274bf74ef9fb11
R4IN <logged2fa@gmail.com>
2025-12-27 17:20:59 -0600

    Create WILTEL.php

---
+++ InfoBase/DataRom/Portal/WILTEL.php
@@ -0,0 +1,33 @@
+#EXTM3U
+#EXTINF:-1 group-title="PlaymeesWilTeles" group-logo="logo" tvg-logo="logo", TN
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=https://apps.playme.com.ar/drm/wv/license
+https://apps.playme.com.ar/dash/CH_TN/playlist.mpd
+#EXTINF:-1 group-title="PlaymeesWilTeles" group-logo="logo" tvg-logo="logo", TN
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=https://apps.playme.com.ar/drm/wv/license
+https://apps.playme.com.ar/dash/CH_TN/playlist.mpd?csi=35613537633931632d353538642d343065322d613363622d663330386535363635353931235745422331373636383737313336
+#EXTINF:-1 group-title="PlaymeesWilTeles" group-logo="logo" tvg-logo="logo", TN
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=b021db96fd1b471993389f1dd7c30c00:48a10bb1f5e099957565a06915460e57
+https://apps.playme.com.ar/dash/CH_TN/playlist.mpd
+#EXTINF:-1 group-title="PlaymeesWilTeles" group-logo="logo" tvg-logo="logo", TN
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=b021db96fd1b471993389f1dd7c30c00:48a10bb1f5e099957565a06915460e57
+https://apps.playme.com.ar/dash/CH_TN/playlist.mpd?csi=35613537633931632d353538642d343065322d613363622d663330386535363635353931235745422331373636383737313336
+#EXTINF:-1 group-title="PlaymeesWilTeles" group-logo="logo" tvg-logo="logo", TN
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=https://apps.playme.com.ar/drm/wv/license
+https://cors-proxy.cooks.fyi/https://apps.playme.com.ar/dash/CH_TN/playlist.mpd
+#EXTINF:-1 group-title="PlaymeesWilTeles" group-logo="logo" tvg-logo="logo", TN
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=https://apps.playme.com.ar/drm/wv/license
+https://cors-proxy.cooks.fyi/https://apps.playme.com.ar/dash/CH_TN/playlist.mpd?csi=35613537633931632d353538642d343065322d613363622d663330386535363635353931235745422331373636383737313336
+#EXTINF:-1 group-title="PlaymeesWilTeles" group-logo="logo" tvg-logo="logo", TN
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=b021db96fd1b471993389f1dd7c30c00:48a10bb1f5e099957565a06915460e57
+https://cors-proxy.cooks.fyi/https://apps.playme.com.ar/dash/CH_TN/playlist.mpd
+#EXTINF:-1 group-title="PlaymeesWilTeles" group-logo="logo" tvg-logo="logo", TN
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=b021db96fd1b471993389f1dd7c30c00:48a10bb1f5e099957565a06915460e57
+https://cors-proxy.cooks.fyi/https://apps.playme.com.ar/dash/CH_TN/playlist.mpd?csi=35613537633931632d353538642d343065322d613363622d663330386535363635353931235745422331373636383737313336
