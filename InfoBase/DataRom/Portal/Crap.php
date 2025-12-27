7f5782ee47674ceadbebf9b658c42aa0e9d259f8
R4IN <logged2fa@gmail.com>
2025-12-27 04:21:38 -0600

    Update and rename BitchMonkey.php to Crap.php

--- InfoBase/DataRom/Portal/Crap.php
+++ InfoBase/DataRom/Portal/Crap.php
@@ -2,4 +2,4 @@
 #EXTINF:-1 group-title="KKK NIGGERS" group-logo="logo" tvg-logo="logo", TSN1
 #KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
 #KODIPROP:inputstream.adaptive.license_key=https://license.9c9media.ca/widevine
-https://live-de.video.9c9media.com/f/TSN/TSN1/manifest.mpd
+http://138.197.129.179:7000/https://live-de.video.9c9media.com/f/ctv/usanetwork/manifest.mpd
