1db7ff0c7ed0f76e8459dd52b7ad29918cb6cd3b
R4IN <logged2fa@gmail.com>
2026-01-02 22:28:08 -0600

    Create dgos.php

---
+++ InfoBase/DataRom/Portal/dgos.php
@@ -0,0 +1,11 @@
+#EXTM3U
+#EXTINF:-1 group-title="DGO" tvg-logo="logo", AMC Series
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"dfc77b4e965f5a638e76e0203782e41b":"fc942e3d37ce67a81979a7277e81d61d"}
+http://atlas.cdmx.gob.mx:8089/https://1161-tbx.vrioott.com/maschic/manifest.mpd
+#EXTINF:-1 group-title="DGO" tvg-logo="logo", Cine Latino
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"d9fa54005f23593b8a5ea3a610715285":"f976a85e90c20ed5c012d31d22d2f305"}
+http://atlas.cdmx.gob.mx:8089/https://1045-tbx.vrioott.com/cinelatino/manifest.mpd
